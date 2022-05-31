cb7fd118270d0a722b7837ba8f8f341aae6931cd
dantran16 <dantranux@gmail.com>
2022-05-31 03:47:22 -0700

    Changed some functions to match the program requirements, but isn't done yet

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -2,11 +2,8 @@ import React, { useState } from "react";
 import "./calculator.css";
 
 const Calculator = () => {
-	const [display, setDisplay] = useState("0");
-	const [history, setHistory] = useState("\u00A0");
-	const [operator, setOperator] = useState("");
-	const [solved, setSolved] = useState(false); // This is used to track if someone doesn't change the result by accident.
-	const [changeOperator, setChangeOperator] = useState(false); // This is used to track if someone wants to change operators after putting in an operator but already put numbers in
+	const [result, setResult] = useState("\u00A0");
+	const [equation, setEquation] = useState("0");
 
 	// Helper Array to map out the buttons. Can be changed into an array of objects if needed!
 	const buttonValues = [
@@ -17,7 +14,7 @@ const Calculator = () => {
 		7,
 		8,
 		9,
-		"X",
+		"*",
 		4,
 		5,
 		6,
@@ -33,99 +30,75 @@ const Calculator = () => {
 
 	const renderButtons = (array) => {
 		// Handles the event for one digit buttons (depending on its value) and concatenates to the result
-		const handleDigitClick = (value) => {
-			setDisplay((prev) => {
-				return prev === "0" || prev === "undefined" || solved
-					? `${value}`
-					: `${prev}${value}`;
+		const handleDigit = (value) => {
+			setEquation((prev) => {
+				return prev === "0" ? `${value}` : `${prev}${value}`;
 			});
-			setSolved(false);
 		};
 
 		// Handles the AC Button to clear all state
 		const handleReset = () => {
-			setDisplay("0");
-			setHistory("\u00A0");
-			setOperator("");
-			setChangeOperator(false);
+			setResult("\u00A0");
+			setEquation("0");
 		};
 
 		// Handles the +/- sign to flip the current operand
-		const handleFlip = () => {
-			setDisplay((prev) => {
-				if (prev.length === 1 && prev[0] === "0") return "0";
-				return prev[0] === "-" ? prev.slice(1) : `-${prev}`;
-			});
-		};
+    const handleFlip = () => {
+      setEquation((prev) => {
+        return prev;
+      })
+    };
 
 		// Concatenates a decimal to the current operand
 		const handleDot = () => {
-			if (solved) return;
-			setDisplay((prev) => {
-				return prev.includes(".") ? prev : `${prev}.`;
+			setEquation((prev) => {
+				const array = prev.split(" ");
+        if(array.length > 1 && "+-*/".includes(array[array.length - 2])) array[array.length - 1] = "0"
+				array[array.length - 1] = array[array.length - 1].includes(".")
+					? array[array.length - 1]
+					: `${array[array.length - 1]}.`;
+				return array.join(" ");
 			});
 		};
 
 		// Handles the deletion of one character in the operand
 		const handleDelete = () => {
-      if (solved) {
-        setSolved(false)
-        setDisplay("0");
-        return;
-      };
-			setDisplay((prev) =>
-				prev.length > 1
-					? prev[0] === "-" && prev.length === 2
-						? "0"
-						: prev.slice(0, prev.length - 1)
-					: "0"
-			);
+			setEquation((prev) => {
+				if (prev === "0" || prev.length < 2) return "0";
+				if ("+-*/".includes(prev[prev.length - 2])) {
+					return prev.slice(0, prev.length - 3);
+				}
+				return prev.slice(0, prev.length - 1);
+			});
 		};
 
 		// Handle the operator
 		const handleOperator = (e) => {
-			if (display.includes("undefined")) return;
-			setOperator(e);
-			setHistory((prev) => {
-				if (operator) return [...prev].slice(0, prev.length - 1).join("") + e;
-				return `${display} ${e}`;
+      setEquation((prev) => {
+        if(prev[prev.length - 1] === ".") return `${prev}0 ${e} `
+				if ("+-*/".includes(prev[prev.length - 2]))
+					return (
+						prev.slice(0, prev.lastIndexOf(prev[prev.length - 2])) +
+						e +
+						prev.slice(prev.lastIndexOf(prev[prev.length - 1]))
+					);
+				return `${prev} ${e} `;
 			});
-			setDisplay(changeOperator ? display : "0");
-			setChangeOperator(true);
 		};
 
 		// Handles the calculations
-		const handleResult = () => {
-			if (!operator) return;
-			setDisplay((prev) => {
-				const equation = `${history} ${prev}`;
-				const [num1, num2] = equation
-					.split(` ${operator} `)
-          .map((e) => Number(e));
-        setHistory(`${num1} ${operator} ${num2} =`);
-				if (operator === "X") return `${num1 * num2}`;
-				else if (operator === "/")
-					return num2 === 0 ? "undefined" : `${num1 / num2}`;
-				else if (operator === "-") return `${num1 - num2}`;
-				else if (operator === "+") return `${num1 + num2}`;
-				return prev;
-			});
-			
-			setOperator("");
-			setSolved(true);
-			setChangeOperator(false);
-		};
+		const handleResult = () => {};
 
 		return array.map((e, i) => {
 			let handleClick = "";
 			// Assigning the right handleClicks
-			if (typeof e === "number") handleClick = () => handleDigitClick(e);
+			if (typeof e === "number") handleClick = () => handleDigit(e);
 			else if (e === "AC") handleClick = () => handleReset();
 			else if (e === "+/-") handleClick = () => handleFlip();
 			else if (e === ".") handleClick = () => handleDot();
 			else if (e === "DEL") handleClick = () => handleDelete();
 			else if (e === "/") handleClick = () => handleOperator(e);
-			else if (e === "X") handleClick = () => handleOperator(e);
+			else if (e === "*") handleClick = () => handleOperator(e);
 			else if (e === "-") handleClick = () => handleOperator(e);
 			else if (e === "+") handleClick = () => handleOperator(e);
 			else if (e === "=") handleClick = () => handleResult();
@@ -147,8 +120,8 @@ const Calculator = () => {
 	return (
 		<div className="container">
 			<div className="calculator--display">
-				<div className="calculator--display--history">{history}</div>
-				<div className="calculator--display--current">{display}</div>
+				<div className="calculator--display--equation">{equation}</div>
+				<div className="calculator--display--result">{result}</div>
 			</div>
 			<div className="calculator--keys">{renderButtons(buttonValues)}</div>
 		</div>
